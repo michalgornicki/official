6c1f7ac7878597af1eeea6d144128159cd6335fa
Michal Gornicki <gornickimichal@gmail.com>
2021-04-08 11:00:09 +0200

    new commit

---
+++ .history/src/components/contact.component_20210408104718.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div id="container5">
+      <h2 class="header header-lower" data-aos="fade">
+        Contact
+      </h2>
+      <div className="contact-wrapper" data-aos="zoom-out">
+        <div className="contact">
+          {" "}
+          <a href="mailto:gornickimichal@gmail.com">Gmail</a>
+        </div>
+        <div className="contact contact-right">
+          {" "}
+          <a href="m.me/micgornicki">Messenger</a>
+        </div>
+        <div className="contact">
+          {" "}
+          <a href="https://www.facebook.com/micgornicki">Facebook</a>
+        </div>
+        <div className="contact contact-right">
+          {" "}
+          <a href="https://github.com/michalgornicki">Github</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;

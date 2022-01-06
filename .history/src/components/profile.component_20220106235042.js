0989850ff5b4805a6b41f287c2e09526a352192f
Michal Gornicki <gornickimichal@gmail.com>
2022-01-07 00:04:27 +0100

    new commit

--- .history/src/components/profile.component_20220106235042.js
+++ .history/src/components/profile.component_20220106235042.js
@@ -12,11 +12,9 @@ const Profile = () => {
         <div className="part part5"></div>
       </div>
 
-      
-
       <div className="text-wrapper" onMouseEnter={() => document.getElementsByClassName("wallpaper")[0].style.filter="blur(5px)"} >
         <h2 className="medium">Michał Górnicki</h2>
-        <h1 className="large" onClick={() => document.getElementsByClassName("large")[0].style.filter= "hue-rotate(" + Math.floor(Math.random() * 1000) + "deg)"}>Front-end developer</h1>
+        <h1 className="large" onClick={() => document.getElementsByClassName("large")[0]}>Front-end developer</h1>
         <h3 className="small">Self-taught, skilled, helpful and proactive front-end developer since 2019. Creating projects from templates or from scratch.</h3>
       </div>
     </div>
